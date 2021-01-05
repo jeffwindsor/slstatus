cd08cf3950f602a02004c5a20dc30223c67dd705
Jeff Windsor <jeff.windsor@gmail.com>
2021-01-04 20:27:02 -0800

    simple

--- config.h
+++ config.h
@@ -63,9 +63,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-		{ cpu_perc, "[CPU %s%%]  ", NULL},
-		{ ram_perc, "[RAM %s%%]  ", NULL},
-		/*{ disk_free,"[hdd %sB]  | ",      "/" },*/
-		{ datetime, "%s","%a %b %d %R" },
-        { battery_perc, "%s", "BAT1"}
+		//{ cpu_perc, " [CPU %s%%]", NULL},
+		//{ ram_perc, " [RAM %s%%]", NULL},
+		//{ disk_free,"[hdd %sB]  | ",      "/" },
+		{ datetime, " %s","%a %b %d %R" },
+        { battery_perc, " [%s%%]", "BAT1"}
 };
